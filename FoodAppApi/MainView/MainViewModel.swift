7593ce656dff51d02f343b6d32d84fb8c690fe9f
Eduardo Chavez <mccartney3150@gmail.com>
2020-12-16 01:58:08 -0600

    Initial combine implementation

--- FoodAppApi/MainView/MainViewModel.swift
+++ FoodAppApi/MainView/MainViewModel.swift
@@ -1,17 +1,35 @@
 import Foundation
+import Combine
 
 protocol MainViewModelProtocol {
-    func getRecipes() -> String
+    func getRandomListRecipes()
+    var recipesPublished: Published<RecipeMain>.Publisher { get }
 }
 
 class MainViewModel: MainViewModelProtocol {
-    let apiProvider: ApiProviderProtocol
+    private let apiProvider: ApiProviderProtocol
     
+    @Published private var recipes: RecipeMain = RecipeMain(recipes: [])
+    
+    var recipesPublished: Published<RecipeMain>.Publisher { $recipes }
+    
+    private var cancellables: Set<AnyCancellable> = []
+
     init(apiProvider: ApiProviderProtocol) {
         self.apiProvider = apiProvider
     }
     
-    func getRecipes() -> String {
-        apiProvider.getRecipes()
+    func getRandomListRecipes() {
+         apiProvider
+            .getRandomRecipes()
+            .result()
+            .sink { [weak self] response in
+                switch response {
+                case .success(let value):
+                    self?.recipes = value
+                case .failure(let error):
+                    print(error)
+                }
+            }.store(in: &cancellables)
     }
 }
